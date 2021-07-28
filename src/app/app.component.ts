4d8584b44846b341db49fa6aa9715400f0cbd596
dandeduck <danielmkhlv@gmail.com>
2021-07-28 09:21:54 +0300

    added existing code

---
+++ src/app/app.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { AmbientLight, Camera, DirectionalLight, Object3D, PerspectiveCamera, Scene, sRGBEncoding, WebGLRenderer } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  private renderer? : WebGLRenderer;
+  private scene : Scene;
+  private loader : GLTFLoader;
+  private camera : Camera;
+
+  private model? : Object3D;
+  private angle = 0;
+
+  private controls? : OrbitControls;
+
+  constructor () {  
+    this.scene = new Scene();
+    this.loader = new GLTFLoader();
+    this.camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 20);
+
+    this.camera.position.setZ(5);
+
+    this.setupLights();
+
+  }
+  
+  ngOnInit(): void {
+    const canvas = <HTMLCanvasElement> document.querySelector('#view');
+    const that = this;
+    
+    this.renderer = new WebGLRenderer({
+      canvas: canvas,
+      alpha: true,
+      // antialias: true
+    });
+    this.renderer.outputEncoding = sRGBEncoding;
+    this.renderer.setPixelRatio(3);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.controls = new OrbitControls(this.camera, canvas);
+    this.controls.update();
+    
+    this.loader.load( 'assets/oil_purifier_DE_second.glb', function ( gltf ) {
+      that.model = gltf.scene;
+      that.model.position.setY(-1.5);
+      
+      that.scene.add(that.model);
+      that.animate();
+    }, function ( xhr ) {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    }, function ( error ) {
+      console.error( error );
+    } );
+  }
+
+  private setupLights() {
+    const light = new AmbientLight(0xffffff);
+    const directionalLight1 = new DirectionalLight( 0xffffff, 0.7 );
+    directionalLight1.position.set(0, 1.1, 0);
+    directionalLight1.lookAt(0, 0, 0);
+    
+    const directionalLight2 = new DirectionalLight( 0xffffff, 0.5 );
+    directionalLight2.position.set(1, 0, 0);
+    directionalLight2.lookAt(0, 0, 0);
+    const directionalLight3 = new DirectionalLight( 0xffffff, 0.5 );
+    directionalLight3.position.set(-1, 0, 0);
+    directionalLight3.lookAt(0, 0, 0);
+    const directionalLight4 = new DirectionalLight( 0xffffff, 0.5 );
+    directionalLight4.position.set(0, 0, 1);
+    directionalLight4.lookAt(0, 0, 0);
+    const directionalLight5 = new DirectionalLight( 0xffffff, 0.5 );
+    directionalLight5.position.set(0, 0, -1);
+    directionalLight5.lookAt(0, 0, 0);
+    
+    this.scene.add(light);
+    this.scene.add(directionalLight1);
+    this.scene.add(directionalLight2);
+    this.scene.add(directionalLight3);
+    this.scene.add(directionalLight4);
+    this.scene.add(directionalLight5);
+  }
+
+  private animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    
+    this.controls?.update();
+    this.renderer?.render(this.scene, this.camera);
+  }
+}
