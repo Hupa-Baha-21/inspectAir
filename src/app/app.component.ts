f1b5ab968511a187ce6218e438df64d464ab3325
dandeduck <danielmkhlv@gmail.com>
2021-10-04 10:52:58 +0300

    Went back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   loadingPercentage = 0;
 
   private config: ModelConfig = {
+    exposure: 2,
     distanceFromModel: 5,
     modelPath: 'assets/oil.glb',
     modelHeight: 1.5,
@@ -32,7 +33,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void { 
     const canvas = <HTMLCanvasElement> document.querySelector('#view');
 
-    this.modelService.setHdrEnvironment('assets/light.hdr');
+    // this.modelService.setHdrEnvironment('assets/light.hdr');
     // this.modelService.setLdrBackground('assets/env.jpg');
     this.modelService.createModelView(canvas, this.config);
     
